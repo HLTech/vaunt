8475e981f73d990e985c1a8540e3a336c1b2e109
Filip Łazarski <filip.lazarski@hltech.com>
2019-04-12 14:33:20 +0200

    Expectations specified

---
+++ vaunt-generator/src/main/java/com/hltech/vaunt/generator/domain/Expectations.java
@@ -0,0 +1,9 @@
+package com.hltech.vaunt.generator.domain;
+
+import lombok.Data;
+
+@Data
+public class Expectations {
+    private final String providerName;
+    private final Contract contract;
+}
