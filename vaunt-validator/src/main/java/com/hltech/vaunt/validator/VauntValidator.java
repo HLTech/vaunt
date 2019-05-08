f27521b6730f47d14c31fe2591663d3e4709a34e
Filip Łazarski <filip.lazarski@hltech.com>
2019-05-08 11:25:11 +0200

    Added description of validation result

--- vaunt-validator/src/main/java/com/hltech/vaunt/validator/VauntValidator.java
+++ vaunt-validator/src/main/java/com/hltech/vaunt/validator/VauntValidator.java
@@ -24,24 +24,32 @@ public class VauntValidator {
 
     private ValidationResult validateWithMatchingProviderContract(
             Contract consumerContract, List<Contract> providerContracts) {
-        Optional<Contract> matchingProviderContract = providerContracts.stream()
+        List<Contract> contracts = providerContracts.stream()
                 .filter(providerContract -> isEndpointMatching(consumerContract, providerContract))
+                .collect(Collectors.toList());
+
+        if (contracts.isEmpty()) {
+            return ValidationResult.failure(consumerContract, providerContracts, ValidationError.MISSING_ENDPOINT);
+        }
+
+        Optional<Contract> matchingProviderContract = contracts.stream()
+                .filter(providerContract -> isSchemaMatching(consumerContract, providerContract))
                 .findFirst();
 
         if (!matchingProviderContract.isPresent()) {
-            return ValidationResult.failure(ValidationError.MISSING_ENDPOINT);
+            return ValidationResult.failure(consumerContract, providerContracts, ValidationError.WRONG_SCHEMA);
         }
 
-        if (!consumerContract.getBody().equals(matchingProviderContract.get().getBody())) {
-            return ValidationResult.failure(ValidationError.WRONG_SCHEMA);
-        }
-
-        return ValidationResult.success();
+        return ValidationResult.success(consumerContract, providerContracts);
     }
 
     private boolean isEndpointMatching(Contract firstContract, Contract secondContract) {
         return firstContract.getDestinationType().equals(secondContract.getDestinationType())
                 && firstContract.getDestinationName().equals(secondContract.getDestinationName());
+    }
+
+    private boolean isSchemaMatching(Contract firstContract, Contract secondContract) {
+        return firstContract.getBody().equals(secondContract.getBody());
     }
 
 }
