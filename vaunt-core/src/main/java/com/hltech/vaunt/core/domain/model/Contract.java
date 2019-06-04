5c0eb16ca8f68cebe8421265fa91564151b9e2f2
Filip Łazarski <filip.lazarski@hltech.com>
2019-06-04 17:03:27 +0200

    toString implementation added so that JsonSchema is better displayed

--- vaunt-core/src/main/java/com/hltech/vaunt/core/domain/model/Contract.java
+++ vaunt-core/src/main/java/com/hltech/vaunt/core/domain/model/Contract.java
@@ -1,6 +1,7 @@
 package com.hltech.vaunt.core.domain.model;
 
 import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
+import com.hltech.vaunt.core.VauntSerializer;
 import lombok.Data;
 
 @Data
@@ -8,4 +9,11 @@ public class Contract {
     private final DestinationType destinationType;
     private final String destinationName;
     private final JsonSchema body;
+
+    @Override
+    public String toString() {
+        return "Contract(destinationType=" + destinationType.toString()
+                + ", destinationName=" + destinationType
+                + ", body =" + new VauntSerializer().serializeSchema(body);
+    }
 }
