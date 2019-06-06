6c10cc950165d7cda0ca0bfb0cbc0ba9591e1b19
Filip Łazarski <filip.lazarski@hltech.com>
2019-06-06 12:54:32 +0200

    Parser of contracts added

--- vaunt-core/src/main/java/com/hltech/vaunt/core/VauntSerializer.java
+++ vaunt-core/src/main/java/com/hltech/vaunt/core/VauntSerializer.java
@@ -1,15 +1,19 @@
 package com.hltech.vaunt.core;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.guava.GuavaModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
 import com.fasterxml.jackson.module.jsonSchema.JsonSchemaGenerator;
+import com.google.common.collect.Lists;
+import com.hltech.vaunt.core.domain.model.Contract;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class VauntSerializer {
 
@@ -33,8 +37,17 @@ public class VauntSerializer {
             return mapper.writeValueAsString(schema);
         } catch (JsonProcessingException ex) {
             ex.printStackTrace();
+            return ""; //TODO: handle
         }
-        return ""; //TODO: handle
+    }
+
+    public List<Contract> parseContracts(String contracts) {
+        try {
+            return mapper.readValue(contracts, new TypeReference<List<Contract>>(){});
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return Lists.newArrayList(); //TODO: handle
+        }
     }
 
     public void serializeToFile(File resultFile, Object value) throws IOException {
