90cf50b283331d4dc2c4f5d9c6ee19d697934b03
Filip Łazarski <filip.lazarski@hltech.com>
2019-06-07 12:46:42 +0200

    Message names (ids) must be the same in validation, in case of inner objects not

--- vaunt-validator/src/test/groovy/com/hltech/vaunt/validator/AnotherSampleMessage.groovy
+++
@@ -1,6 +0,0 @@
-package com.hltech.vaunt.validator
-
-class AnotherSampleMessage {
-    String value
-    int amount
-}
---
+++ vaunt-validator/src/test/groovy/com/hltech/vaunt/validator/projectB/messages/SampleMessage.groovy
@@ -0,0 +1,23 @@
+package com.hltech.vaunt.validator.projectB.messages
+
+class SampleMessage {
+    String value
+    int amount
+    InnerMessage innerMessage
+}
+
+class SameMessageAsSample {
+    String value
+    int amount
+    InnerMessage innerMessage
+}
+
+class InnerMessage {
+    String value2
+    BigDecimal amount
+}
+
+class AnotherSampleMessage {
+    String value
+    int amount
+}
