d17c63d1e74fcd58fcc0c152be1ea51b2853b7b2
Filip Łazarski <filip.lazarski@hltech.com>
2019-06-17 21:03:42 +0200

    Multiple @Consumer or @Producer on one class allowed

--- vaunt-generator/src/main/java/com/hltech/vaunt/generator/domain/representation/annotation/Consumer.java
+++ vaunt-generator/src/main/java/com/hltech/vaunt/generator/domain/representation/annotation/Consumer.java
@@ -3,10 +3,12 @@ package com.hltech.vaunt.generator.domain.representation.annotation;
 import com.hltech.vaunt.core.domain.model.DestinationType;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Repeatable(Consumers.class)
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Consumer {
--- vaunt-generator/src/test/groovy/com/hltech/vaunt/generator/domain/representation/message/poliannotated/SampleProviderMessage.java
+++ vaunt-generator/src/test/groovy/com/hltech/vaunt/generator/domain/representation/message/poliannotated/SampleProviderMessage.java
@@ -1,10 +1,11 @@
-package com.hltech.vaunt.generator.domain.representation.message;
+package com.hltech.vaunt.generator.domain.representation.message.poliannotated;
 
 import com.hltech.vaunt.generator.domain.representation.annotation.Provider;
 import com.hltech.vaunt.core.domain.model.DestinationType;
 import lombok.Data;
 
 @Provider(destinationType = DestinationType.QUEUE, destinationName = "destination")
+@Provider(destinationType = DestinationType.QUEUE, destinationName = "destination2")
 @Data
 public class SampleProviderMessage {
     private final long value;
