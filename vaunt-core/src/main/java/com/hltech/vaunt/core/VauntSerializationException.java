cbc55e0f41b1603de890b5d31174f965697a1321
Filip Łazarski <filip.lazarski@hltech.com>
2019-08-12 12:00:14 +0200

    VauntSerializationException introduced

---
+++ vaunt-core/src/main/java/com/hltech/vaunt/core/VauntSerializationException.java
@@ -0,0 +1,8 @@
+package com.hltech.vaunt.core;
+
+public class VauntSerializationException extends RuntimeException {
+
+    public VauntSerializationException(String message, Throwable reason) {
+        super(message, reason);
+    }
+}
