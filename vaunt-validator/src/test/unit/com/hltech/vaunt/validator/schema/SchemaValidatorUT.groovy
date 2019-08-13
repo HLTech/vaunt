42f8d04b20df23ed2f59a7333027215fa8ed501e
Filip Łazarski <filip.lazarski@hltech.com>
2019-08-13 12:40:57 +0200

    If for any type there is no validator, exception is thrown

--- vaunt-validator/src/test/unit/com/hltech/vaunt/validator/schema/SchemaValidatorUT.groovy
+++ vaunt-validator/src/test/unit/com/hltech/vaunt/validator/schema/SchemaValidatorUT.groovy
@@ -1,7 +1,9 @@
 package com.hltech.vaunt.validator.schema
 
 import com.fasterxml.jackson.module.jsonSchema.types.IntegerSchema
+import com.fasterxml.jackson.module.jsonSchema.types.NullSchema
 import com.fasterxml.jackson.module.jsonSchema.types.StringSchema
+import com.hltech.vaunt.validator.VauntValidationException
 import spock.lang.Specification
 import spock.lang.Subject
 
@@ -25,7 +27,7 @@ class SchemaValidatorUT extends Specification {
             validator.validate(new StringSchema(), new StringSchema()) != null
     }
 
-    def 'Should throw exception when no validator is found for given consumer and provider schemas'() {
+    def 'Should return error when consumer has different schema than provider'() {
         when:
             def errors = validator.validate(new StringSchema(id: 'a'), new IntegerSchema(id: 'b'))
 
@@ -33,5 +35,15 @@ class SchemaValidatorUT extends Specification {
             errors
             errors.size() == 1
             errors[0] == 'Consumer schema with id a and type StringSchema does not match provider schema with id b and type IntegerSchema'
+    }
+
+    def 'Should throw exception when no validator found for given schema'() {
+        when:
+            def errors = validator.validate(new NullSchema(id: 'a'), new NullSchema(id: 'b'))
+
+        then:
+            errors == null
+            def ex = thrown VauntValidationException
+            ex.getMessage() == 'Exactly one validator should exist for consumer and provider of type NullSchema'
     }
 }
